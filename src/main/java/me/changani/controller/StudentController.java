4ab0c459b2dbe2de84299da6b4ebae03015c3f37
Firmino Changani <firmino.changani@gmail.com>
2021-01-29 21:11:54 +0000

    fix: Remove unnecessary code

--- src/main/java/me/changani/controller/StudentController.java
+++ src/main/java/me/changani/controller/StudentController.java
@@ -27,90 +27,44 @@ import me.changani.service.StudentService;
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-	
-	@Value("${app.name}")
-	private String appName;
-	
+
 	@Autowired
 	StudentService studentService;
-	
 
-	@GetMapping("")
-	public List<StudentResponse> getAllStudents() {
-		 List<Student> listStudents = studentService.getAllStudents();
-		 List<StudentResponse> studentReponseList = new ArrayList<StudentResponse>();
-		 
-		 listStudents.stream().forEach(student -> {
-			 studentReponseList.add(new StudentResponse(student));
-		 });
-		 
-		 return studentReponseList;
-	}
-	
-	@PostMapping("")
-	public StudentResponse createStudent(@Valid @RequestBody  CreateStudentRequest student) {
-		Student newStudent = studentService.createStudent(student);
+	private List<StudentResponse> prepareStudentsResponse(List<Student> students) {
+		List<StudentResponse> listOfStudents = new ArrayList<StudentResponse>();
 		
+		students.stream().forEach(student -> {
+			listOfStudents.add(new StudentResponse(student));
+		});
+
+		return listOfStudents;
+	}
+
+	@GetMapping
+	public List<StudentResponse> getAllStudents(@RequestParam("page") int page, @RequestParam("per_page") int perPage) {
+		List<Student> students = studentService.getAllStudents(page, perPage);
+		
+		return this.prepareStudentsResponse(students);
+	}
+
+	@PostMapping
+	public StudentResponse createStudent(@Valid @RequestBody CreateStudentRequest student) {
+		Student newStudent = studentService.createStudent(student);
+
 		return new StudentResponse(newStudent);
 	}
-	
-	@PutMapping("")
-	public StudentResponse updateStudent(@Valid @RequestBody UpdateStudentRequest updateStudentRequest) {
-		Student student = studentService.updateStudent(updateStudentRequest);
-		
+
+	@PutMapping("{id}")
+	public StudentResponse updateStudent(@PathVariable("id") Long id,
+			@Valid @RequestBody UpdateStudentRequest updateStudentRequest) {
+		Student student = studentService.updateStudent(id, updateStudentRequest);
+
 		return new StudentResponse(student);
 	}
-	
+
 	@DeleteMapping("{id}")
-	public void deleteStudent(@PathVariable("id")  long id) {
+	public void deleteStudent(@PathVariable("id") long id) {
 		studentService.deleteStudent(id);
-	}
-	
-	@GetMapping("getByFirstName/{firstName}")
-	public List<StudentResponse> getStudentByName(@PathVariable("firstName") String firstName) {
-		return studentService.getStudentsByFirstName(firstName);
-	}
-	
-	@GetMapping("getByFirstNameAndLastName/{firstName}/{lastName}")
-	public List<StudentResponse> getStudentByFirstNameAndLastName(
-			@PathVariable("firstName") String firstName,
-			@PathVariable("lastName") String lastName
-	) {
-		return studentService.getStudentByFirstNameAndLastName(firstName, lastName);
-	}
-	
-	@GetMapping("getByFirstNameOrLastName/{firstName}/{lastName}")
-	public List<StudentResponse> getStudentByFirstNameOrLastName(
-			@PathVariable("firstName") String firstName,
-			@PathVariable("lastName") String lastName
-	) {
-		return studentService.getSudentByFirstNameOrLastName(firstName, lastName);
-	}
-	
-	@GetMapping("getByFirstNameIn")
-	public List<StudentResponse> getStudentByFirstName(@RequestBody InQueryRequest inQueryRequest) {
-		return studentService.getSudentByFirstNameIn(inQueryRequest);
-	}
-	
-	@GetMapping("getWithPagination")
-	public List<StudentResponse> getStudentWithPagination(
-			@RequestParam("per_page") int perPage,
-			@RequestParam("page") int page
-	){
-		return studentService.getSudentWithPagination(perPage, page);
-	}
-	
-	@GetMapping("getAllWithSorting")
-	public List<StudentResponse> getAllStudentsWithSorting() {
-		return studentService.getSudentWithSort();
-	}
-	
-	@PutMapping("updateFirstName/{id}/{firstName}")
-	public String updateFirstName(
-			@PathVariable("id") Long id,
-			@PathVariable("firstName") String firstName
-	) {
-		Integer numberOfUpdatedRecords = studentService.updateStudentFirstName(id, firstName);
-		return numberOfUpdatedRecords + " was/were updated successfully";
 	}
 }
